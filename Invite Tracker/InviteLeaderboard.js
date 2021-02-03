1b7c39f5e1e93996e37cb71032a53631081c6e51
Adler <73528181+AdlerVaz@users.noreply.github.com>
2021-02-03 11:09:12 +0530

    Invite Leaderboard

---
+++ Invite Tracker/InviteLeaderboard.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+    name: 'invitelb',
+    description: 'Invite leaderboard',
+    run: async (bot, message, args) =>{
+        const { guild } = message
+      
+        guild.fetchInvites().then((invites) => {
+          const inviteCounter = {
+
+          }
+    
+          invites.forEach((invite) => {
+            const { uses, inviter } = invite
+            const { username, discriminator } = inviter
+    
+            const name = `${username}#${discriminator}`
+    
+            inviteCounter[name] = (inviteCounter[name] || 0) + uses
+          })
+    
+          let replyText = 'Invites:'
+    
+          const sortedInvites = Object.keys(inviteCounter).sort(
+            (a, b) => inviteCounter[b] - inviteCounter[a]
+          )
+    
+          console.log(sortedInvites)
+    
+          if(sortedInvites.length === 10) sortedInvites.length = 10;
+    
+          for (const invite of sortedInvites) {
+            const count = inviteCounter[invite]
+            replyText += `\n${invite} has invited ${count} member(s)!`
+          }
+    
+          message.channel.send(replyText)
+        })
+      },
+    }
